93418dc556063214d4ee375abe61c1e13448c211
aarthip435-design <aarthip435@gmail.com>
2025-12-20 12:13:20 +0530

    Add files via upload

---
+++ ExampleOfVector.java
@@ -0,0 +1,29 @@
+import java.util.Vector;
+public class ExampleOfVector{
+public static void main(String[] args){
+Vector v1=new Vector();
+v1.add(10);
+v1.add(20);
+v1.add(30);
+v1.add("java");
+System.out.println(v1);
+System.out.println("......................................");
+System.out.println(v1.get(2));
+System.out.println("......................................");
+System.out.println(v1.isEmpty());
+System.out.println("......................................");
+System.out.println(v1.contains(2));
+System.out.println("......................................");
+System.out.println(v1.size());
+System.out.println("......................................");
+for(Object i:v1){
+System.out.println(i);
+Vector<String> v2=new Vector<String>();
+v2.add("java script");
+v2add("python");
+v2.add("html");
+v2.add("java");
+System.out.println(v);
+}
+}
+}
