dd7e52067a9dbc61877e846e5059b95d0724b421
aarthip435-design <aarthip435@gmail.com>
2025-12-21 11:54:12 +0530

    Add files via upload

---
+++ ExampleOfTreeSet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ExampleOfTreeSet{
+public static void main(String[] args){
+TreeSet<Integer> ts=new TreeSet<Integer>();
+ts.add(50);
+ts.add(10);
+ts.add(35);
+ts.add(70);
+System.out.println(ts);
+}
+}
