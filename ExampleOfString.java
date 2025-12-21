047b3a601cf5f06834297b965998ea096df1ebf1
aarthip435-design <aarthip435@gmail.com>
2025-12-21 14:05:50 +0530

    Add files via upload

---
+++ ExampleOfString.java
@@ -0,0 +1,12 @@
+public class ExampleOfString{
+public static void main(String[] args){
+/*StringBuffer sb=new StringBuffer("Aarthi");
+System.out.println(sb);
+sb.append(" Prabhas");
+System.out.println(sb);*/
+StringBuilder s=new StringBuilder("Aarthi");
+System.out.println(s);
+s.append(" Prabhas");
+System.out.println(s);
+}
+}
